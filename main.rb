94f3913e8a1392f817d9a4af4b02a395c9cadb07
Longkai CAO <chineseneo1@gmail.com>
2015-03-15 11:23:56 +0800

    update site seo

--- main.rb
+++ main.rb
@@ -47,23 +47,26 @@ class Main < Sinatra::Application
   end
 
   get '/' do
-    @page_title = '首页'
     @page_name = 'index'
+    @page_title = '陕西奕诚生物科技有限公司 - 首页'
+    @page_description = '欢迎来到陕西奕诚生物科技有限公司，本公司位于陕西省商洛市镇安县。公司致力于将生物科技融入农业发展，引进国外先进作物，结合食品深加工技术，生产高品质农作物和精致食品。科技融入栗乡，奕诚必出精品。'
     erb :index, :layout => :default,
         :layout_options => {:views => settings.layouts_dir}
   end
 
   get '/information' do
-    @page_title = '公司概况'
     @page_name = 'information'
+    @page_title = '陕西奕诚生物科技有限公司 - 公司概况'
+    @page_description = '陕西奕诚生物科技有限公司与陕西恒富源林业有限公司、陕西秦山核桃专业合作社并肩合作。生产工厂位于秦岭山区镇安县云镇，优质的空气质量加上绿色有机的原材料，用科学的生产工艺生产出来的产品不仅口感香醇更利于健康。'
     erb :information, :layout => :default,
         :layout_options => {:views => settings.layouts_dir}
   end
 
   get '/projects' do
     project_id = params[:project_id] || 1
-    @page_title = '项目情况'
     @page_name = 'projects'
+    @page_title = '陕西奕诚生物科技有限公司 - 公司项目情况'
+    @page_description = '陕西奕诚生物科技有限公司目前正在致力于药用植物及经济作物新型苗孵化基地及推广种植，镇安秦岭山区5000亩树莓基地建设，商洛市镇安县1000亩七叶树基地建设和镇安县板栗种植及深加工产业化等四大项目。'
     erb "projects-#{project_id}".to_sym, :layout => :default,
         :layout_options => {:views => settings.layouts_dir}
   end
